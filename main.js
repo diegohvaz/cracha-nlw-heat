852fb90614ab7d5c2c70d17621031608c1d39a0a
Diêgo <diego.vaz.revolution@gmail.com>
2021-10-21 21:45:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linkSocialMedia = {
+  github: 'diegohvaz',
+  facebook: 'diego.gomes.06',
+  youtube: 'channel/UCvsFmQ2iMTYQt-N95B8k-Pw',
+  instagram: 'diegohvaz',
+  twitter: 'diegohvaaz'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linkSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linkSocialMedia.github}` //Template String
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
